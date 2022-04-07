ea777d860b979b6d44ec601e496503cbfd29f651
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-04-07 06:00:38 +0200

    fix: fixed type serialization to use less reflection

--- src/DynamicContext.cs
+++ src/DynamicContext.cs
@@ -89,25 +89,52 @@ namespace DotNetDevOps.Extensions.EAVFramework
         private static PropertyInfo container = typeof(Microsoft.AspNetCore.OData.Query.Wrapper.ISelectExpandWrapper)
             .Assembly.GetType("Microsoft.AspNetCore.OData.Query.Wrapper.SelectExpandWrapper")?.GetProperty("Container");
 
+        private static MethodInfo getElementType = typeof(Microsoft.AspNetCore.OData.Query.Wrapper.ISelectExpandWrapper)
+           .Assembly.GetType("Microsoft.AspNetCore.OData.Query.Wrapper.SelectExpandWrapper")?.GetMethod("GetElementType", BindingFlags.NonPublic | BindingFlags.Instance);
+
         public SelectSomeOfT(IODataRuntimeTypeFactory oDataRuntimeTypeFactory)
         {
             this.oDataRuntimeTypeFactory=oDataRuntimeTypeFactory;
            
         }
         private ConcurrentDictionary<Type, PropertyInfo> _NamedPropertyBag = new ConcurrentDictionary<Type, PropertyInfo>();
+        private ConcurrentDictionary<string, string> LogicalNameMapping = new ConcurrentDictionary<string, string>();
+
+        public static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = a.GetType(typeName);
+                if (type != null)
+                    return type;
+            }
+            return null;
+        }
 
         public string GetDataType(object data)
         {
-            var instance = untypedInstance?.GetValue(data);
-            if (instance== null)
+            var test = data as  IEdmObject;
+            var modeltype = test.GetEdmType();
+            var serializedType = LogicalNameMapping.GetOrAdd(modeltype.Definition.FullTypeName(), (typename) =>
             {
+                return GetType(typename)?.GetCustomAttribute<EntityAttribute>()?.LogicalName??typename;
 
-                var c = container.GetValue(data);
-                var namedProperty = _NamedPropertyBag.GetOrAdd(c.GetType(), (t) => t.GetProperty("Value"));
-                var n = namedProperty.GetValue(c);
-                instance=untypedInstance.GetValue(n);
-            }
-            var serializedType = instance?.GetType().GetCustomAttribute<EntityAttribute>()?.LogicalName;
+            });
+
+            //var instance = untypedInstance?.GetValue(data);
+            //if (instance== null)
+            //{
+            //    var aa = getElementType.Invoke(data,null);
+
+
+            //    var c = container.GetValue(data);
+            //    var namedProperty = _NamedPropertyBag.GetOrAdd(c.GetType(), (t) => t.GetProperty("Value"));
+            //    var n = namedProperty.GetValue(c);
+            //    instance=untypedInstance.GetValue(n);
+            //}
+            //var serializedType = instance?.GetType().GetCustomAttribute<EntityAttribute>()?.LogicalName;
             return serializedType;
         }
     }
