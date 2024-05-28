6ad52031f0698ebe2b1caa82f13eed5519e619fa
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-05-28 08:54:12 +0200

    resolution on test where attribute simple types not deserialiazble

--- sdk/DTO/AttributeDefinition.cs
+++ sdk/DTO/AttributeDefinition.cs
@@ -71,6 +71,42 @@ namespace EAVFW.Extensions.Manifest.SDK
         public Dictionary<string, JsonElement> AdditionalFields { get; set; }
     }
 
+
+    public class TypeDefinitionConverter : JsonConverter<TypeDefinition>
+    {
+        public override TypeDefinition Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    return new TypeDefinition { Type = reader.GetString() ?? "" };
+                case JsonTokenType.StartObject:
+                    var node = JsonNode.Parse(ref reader);
+                    var t = node.Deserialize<TypeDefinition>();
+                    return t;
+                default:
+                    throw new Exception($"{reader.TokenType} is not supported as AttributeDefinition");
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, TypeDefinition value, JsonSerializerOptions options)
+        {
+            var jsonString = value switch
+            {
+                TypeDefinition attributeObjectDefinition =>
+                    JsonSerializer.Serialize(attributeObjectDefinition),
+                
+                _ => throw new ArgumentException($"{value.GetType()} is not supported")
+            };
+
+            var jsonNode = JsonNode.Parse(jsonString);
+            jsonNode?.WriteTo(writer, options);
+        }
+    }
+    [JsonConverter(typeof(TypeDefinitionConverter))]
     public class TypeDefinition
     {
         [JsonPropertyName("type")] public string Type { get; set; }
